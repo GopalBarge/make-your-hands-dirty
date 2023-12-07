eb08d3996b74018b2b9d6059b240652331570259
Gopal <gopal.barge@ayanworks.com>
2023-12-08 02:09:22 +0530

    swagger integration

--- src/main/java/com/make/your/hands/dirty/common/controller/UserController.java
+++ src/main/java/com/make/your/hands/dirty/common/controller/UserController.java
@@ -3,6 +3,10 @@ package com.make.your.hands.dirty.common.controller;
 // UserController.java
 import com.make.your.hands.dirty.common.entity.User;
 import com.make.your.hands.dirty.common.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +14,18 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@Tag(name = "User API", description = "user management Api")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     @Autowired
     private UserService userService;
-
+    @Operation(
+            summary = "Fetch all users",
+            description = "fetches all user entities and their data from data source")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation")
+    })
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
